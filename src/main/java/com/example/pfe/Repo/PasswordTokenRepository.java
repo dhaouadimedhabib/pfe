9189ec54448070be2ae659b7e953e3ed5bea114c
dhaouadimedhabib <medhabib.dhaouadi@esprit.tn>
2024-08-13 01:08:47 +0200

    update

--- src/main/java/com/example/pfe/Repo/PasswordTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.pfe.Repo;
-
-import com.example.pfe.Domain.PasswordResetToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PasswordTokenRepository extends JpaRepository<PasswordResetToken, Long> {
-    PasswordResetToken findByToken(String token);
-
-}
